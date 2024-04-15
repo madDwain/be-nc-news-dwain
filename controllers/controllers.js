699c9591c7f428de7758ed0b201deeb9094a65fb
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-04-15 14:24:55 +0100

    added a get endpoints endpoint

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,11 +1,18 @@
-const {fetchTopics} = require('../models/topics')
+const { fetchTopics } = require("../models/topics");
 
-function getTopics(req, res ,next) {
-    return fetchTopics().then((topics) => {
-        res.status(200).send(topics)
-    } )
+function getTopics(req, res, next) {
+  return fetchTopics().then((topics) => {
+    res.status(200).send(topics);
+  });
+}
+
+function getEndpoints(req, res, next) {
+    const endpoints = require('../endpoints.json')
+    console.log(typeof endpoints)
+    res.status(200).send({endpoints})
 }
 
 module.exports = {
-    getTopics
-}
+  getTopics,
+  getEndpoints,
+};
