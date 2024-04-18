769f96f03230d5dd8f87d31ec1b4d3d3bfcc2652
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-04-18 12:33:12 +0100

    more refactoring

--- __tests__/test.js
+++ __tests__/test.js
@@ -95,6 +95,7 @@ describe("/api/articles/:article_id", () => {
   describe("PATCH request", () => {
     test("should accept an object in the form { inc_votes: newVote }, returning 200 and the updated article", () => {
       const expectedArticle = {
+        article_id: 1,
         title: "Living in the shadow of a great man",
         topic: "mitch",
         author: "butter_bridge",
@@ -110,7 +111,28 @@ describe("/api/articles/:article_id", () => {
         .expect(200)
         .then(({ body }) => {
           const article = body;
-          expect(article).toMatchObject(expectedArticle);
+          expect(article).toEqual(expectedArticle);
+        });
+    });
+    test("should accept an object in the form { inc_votes: newVote }, returning 200 and the updated article if inc_votes is negative", () => {
+      const expectedArticle = {
+        article_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "2020-07-09T20:11:00.000Z",
+        votes: 90,
+        article_img_url:
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+      };
+      return request(app)
+        .patch("/api/articles/1")
+        .send({ inc_votes: -10 })
+        .expect(200)
+        .then(({ body }) => {
+          const article = body;
+          expect(article).toEqual(expectedArticle);
         });
     });
     test("should return 400 and a message of no new vote object if not passed a newVote object", () => {
@@ -121,14 +143,33 @@ describe("/api/articles/:article_id", () => {
           expect(body.msg).toBe("no new vote object");
         });
     });
-    test("should return 404 and a message of article id not found if artcile id not found", () => {
+    test("should return 404 and a message of article id not found if article id not found", () => {
       return request(app)
         .patch("/api/articles/9999")
+        .send({ inc_votes: 10 })
         .expect(404)
         .then(({ body }) => {
           expect(body.msg).toBe("article id not found");
         });
     });
+    test("should return 400 if article id is not a number", () => {
+      return request(app)
+      .patch("/api/articles/notanumber")
+      .send({ inc_votes: 10 })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe('invalid id type')
+      })
+    })
+    test("should return 400 if inc_votes value is not a number", () => {
+      return request(app)
+      .patch("/api/articles/1")
+      .send({ inc_votes: 'notanumber' })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe('invalid id type')
+      })
+    })
   });
 });
 
